f92a72b1c5d56666c67f744ec23e0f41f7da4d0b
sebsve <sdp.svensson@gmail.com>
2024-05-06 22:47:13 +0200

    added swagger + db schema + fix invalid Entity name

--- src/main/java/dev/sebsve/infrastructue/model/User.java
+++ src/main/java/dev/sebsve/infrastructue/model/User.java
@@ -2,18 +2,22 @@ package dev.sebsve.infrastructue.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 
 @Entity
+@Table(name = "users")
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
+    @UuidGenerator
     private String id;
     private String username;
     private String email;
