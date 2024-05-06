06f027cda6c72b4730ace2bc5f1d45aa991d404d
sebsve <sdp.svensson@gmail.com>
2024-05-06 09:05:54 +0200

    use JpaRepostory instead of Crud

--- src/main/java/dev/sebsve/infrastructue/UserRepository.java
+++ src/main/java/dev/sebsve/infrastructue/UserRepository.java
@@ -1,11 +1,11 @@
 package dev.sebsve.infrastructue;
 
 import dev.sebsve.infrastructue.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     User findByUsername(String username);
     User findByEmail(String email);
 }
